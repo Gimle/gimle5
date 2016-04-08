b356ef63c9ee45ae28f419d489127d663983108f
Tux Solbakk <tux@krageroby.no>
2016-04-08 19:42:18 +0200

    Added base html canvas, and updated names.

--- canvas/html.php
+++ canvas/html.php
@@ -2,19 +2,5 @@
 namespace gimle;
 
 header('Content-type: text/html; charset=' . mb_internal_encoding());
-?>
-<!doctype html>
-<html lang="%lang%">
-	<head>
-		<meta charset="<?=mb_internal_encoding()?>">
-		<meta http-equiv="x-ua-compatible" content="ie=edge">
-		<title>%title%</title>
-		<meta name="description" content="%description%">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-	</head>
-	<body>
-		%content%
-	</body>
-</html>
-<?php
+?>%content%<?php
 return true;
