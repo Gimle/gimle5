5f3c469715b864f9dfa5528602170ff6d97d3213
Tux Solbakk <tux@krageroby.no>
2016-05-18 08:29:21 +0200

    Added get method for exception params.

--- lib/gimle/exception.php
+++ lib/gimle/exception.php
@@ -10,7 +10,16 @@ class Exception extends \Exception
 		$this->params = ArrayUtils::merge($this->params, ArrayUtils::stringToNestedArray($key, $value));
 	}
 
-	public function get ($key)
+	public function get ($key = false)
 	{
+		if ($key === false) {
+			return $this->params;
+		}
+
+		if (isset($this->params[$key])) {
+			return $this->params[$key];
+		}
+
+		return null;
 	}
 }
